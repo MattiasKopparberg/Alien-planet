06c9904c7dddb7a906ac363840dcef5e6c9b09b2
MattiasKopparberg <mkopparberg@hotmail.com>
2026-05-19 15:17:31 +0200

    server and config are done, error on startup due to app.ts not being done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,32 @@
-import "dotenv/config";
+import app from "./app.js";
+import { db } from "./config/db.js";
 
-console.log("Server says hello");
+const PORT = 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
+
+const gracefulShutdown = async () => {
+  console.log("Shutting down gracefully...");
+
+  try {
+
+    await db.end();
+
+    server.close(() => {
+      console.log("Server closed");
+
+      process.exit(0);
+    });
+
+  } catch (err) {
+
+    console.error("Shutdown error:", err);
+
+    process.exit(1);
+  }
+};
+
+process.on("SIGINT", gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
