e76c22fd366f91e3a4f4d4efc6c50d3ee42c9469
MattiasKopparberg <mkopparberg@hotmail.com>
2026-05-20 11:44:08 +0200

    DB_Name can now be alienplanets with newly added adminconnection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,7 +5,7 @@ export const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "",
+  database: "alienplanets",
 
   waitForConnections: true,
   connectionLimit: 10,
