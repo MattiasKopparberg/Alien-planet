6cf05bbb95ca1a1ac75bacdac375759ab3af70fa
MattiasKopparberg <mkopparberg@hotmail.com>
2026-05-20 14:38:06 +0200

    created route endpoints for aliens and planets

---
+++ backend/src/routes/planets.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import { 
+    getPlanets,
+    getPlanetById,
+    getPlanetImage
+    // createPlanet,
+    // updatePlanet,
+    // deletePlanet,
+} from "../controller/planetsController.js"
+
+const router = express.Router();
+
+router.get("/", getPlanets)
+router.get("/:id", getPlanetImage)
+router.get("/:id", getPlanetById)
