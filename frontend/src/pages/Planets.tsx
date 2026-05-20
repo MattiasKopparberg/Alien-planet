5f9da079ca43a0b46c8f39eb705158b5a1efad6d
Rebecka Johansson <rebeckaemmajohansson@gmail.com>
2026-05-20 14:43:26 +0200

    add planets view with mockdata, PlanetCard and List

--- frontend/src/pages/Planets.tsx
+++ frontend/src/pages/Planets.tsx
@@ -1,3 +1,31 @@
+import PlanetCard from "../components/planets/PlanetCard";
+import List from "../components/List";
+import type { Planet } from "../types/types";
+
+const mockDataPlanets: Planet[] = [
+  {
+    planet_id: 1,
+    name: "Mars",
+    surface_area: 144798500,
+    avg_temp: -60,
+  },
+  {
+    planet_id: 2,
+    name: "Venus",
+    surface_area: 460200000,
+    avg_temp: 464,
+  },
+];
+
 export default function Planets() {
-  return <h1>Planets</h1>;
+  return (
+    <div>
+      <h1>Planets</h1>
+      {/* renderItem skickas till List och skapar ett PlanetCard per planet-objekt */}
+      <List
+        items={mockDataPlanets}
+        renderItem={(item) => <PlanetCard planet={item} />}
+      />
+    </div>
+  );
 }
