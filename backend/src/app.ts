bb6bfe729a683e11ae376e29d8f17753d96816e7
MattiasKopparberg <mkopparberg@hotmail.com>
2026-05-20 14:04:32 +0200

    Added app routes and test endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,12 @@
 import express from "express";
+// import planetsRoute from "./routes/planetsRoute"
+// import aliensRoute from "./routes/aliensRoute"
 
 export const app = express();
+
+app.use(express.json())
+// app.use("/aliens/", aliensRoute)
+// app.use("/planets", planetsRoute)
+app.get("/", (req,res) => {
+    console.log({message: "API is running"})
+})
