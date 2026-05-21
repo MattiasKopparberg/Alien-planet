9d7548d87bdffc6ca42570b6437bba3c6d1e8148
Emelie Nilsson Vilör <emelie.vilor@gmail.com>
2026-05-21 12:49:46 +0200

    Added navbar and links on home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Planets from "./pages/Planets";
 import Aliens from "./pages/Aliens";
+import "./App.css";
 
 function App() {
   return (
+    <div className="App">
+    <nav className="flex-3">
+      <Link to="/"><button>Home</button></Link>
+      <Link to="/Planets"><button>Planets</button></Link>
+      <Link to="/Aliens"><button>Aliens</button></Link>
+    </nav>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/planets" element={<Planets />} />
       <Route path="/aliens" element={<Aliens />} />
     </Routes>
+    </div>
   );
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+    <h1>Alien Planets</h1>
+    <h2>Some intro text</h2>
+    <Link to="/Planets"><button>Explore Planets</button></Link>
+    <Link to="/Aliens"><button>Explore Aliens</button></Link>
+    </>
+  );
+
 }
