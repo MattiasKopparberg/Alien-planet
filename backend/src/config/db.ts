49853dec3127c87dc53ab30fddc4706998598290
Linnéa Toth <linnea@liroit.com>
2026-05-21 09:03:56 +0200

    add alienmodels (all, by id, by aggression), types Alien and Aggression, query function, update log

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -11,3 +11,13 @@ export const db = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+export const query = async (sql: string, params?: any[]) => {
+  try {
+    const [rows] = await db.execute(sql, params);
+    return rows;
+  } catch (e) {
+    console.error("Database error: ", e);
+    throw e;
+  }
+};
---
+++ backend/src/models/alienModel.ts
@@ -0,0 +1,38 @@
+import { query } from "../config/db.js";
+import type { Alien, Aggression } from "../types/types.js";
+
+export const findAllAliens = async () => {
+  try {
+    const aliens = (await query(
+      "SELECT * FROM aliens ORDER BY alien_id",
+    )) as Alien[];
+    return aliens;
+  } catch (e) {
+    console.error("Query failed in findAllAliens: ", e);
+    throw e;
+  }
+};
+
+export const findAlienById = async (id: number) => {
+  try {
+    const aliens = (await query("SELECT * FROM aliens WHERE alien_id = ?", [
+      id,
+    ])) as Alien[];
+    return aliens.length > 0 ? aliens[0] : null;
+  } catch (e) {
+    console.error("Query failed in findAlienById: ", e);
+    throw e;
+  }
+};
+
+export const findAlienByAggression = async (aggression: Aggression) => {
+  try {
+    const aliens = (await query("SELECT * FROM aliens WHERE aggression = ?", [
+      aggression,
+    ])) as Alien[];
+    return aliens;
+  } catch (e) {
+    console.error("Query failed in findAlienByAggression: ", e);
+    throw e;
+  }
+};
