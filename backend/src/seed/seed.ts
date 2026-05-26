70c9ef417388994d4907e8680a5d6d12f28ab630
Linnéa Toth <linnea@liroit.com>
2026-05-26 11:36:30 +0200

    add planet img 7-15, update mock data to match

--- backend/src/seed/seed.ts
+++ backend/src/seed/seed.ts
@@ -6,9 +6,9 @@ const dbInitStatements = [
   "CREATE DATABASE alienplanets",
   "USE alienplanets",
   "CREATE TABLE planets (planet_id int PRIMARY KEY AUTO_INCREMENT, name varchar(25) NOT NULL, surface_area float, avg_temp float)",
-  "INSERT INTO planets (name, surface_area, avg_temp) VALUES ('Mercury', 74.8, 167), ('Venus', 460.2, 464), ('Earth', 510.1, 15), ('Mars', 144.8, -65), ('Jupiter', 61420, -110), ('Saturn', 42700, -140), ('Uranus', 8083, -195), ('Neptune', 7618, -200), ('Kepler-22b', 900, 22), ('Proxima-b', 120, -39), ('Gliese-581g', 180, -12), ('Pandora', 95, 18),('LV-426', 110, -45), ('Arrakis', 300, 58), ('Hoth', 250, -60)",
+  "INSERT INTO planets (name, surface_area, avg_temp) VALUES ('Hoth', 250, -60), ('Venus', 460.2, 464), ('Arrakis', 300, 58), ('Pandora', 95, 18), ('Proxima-b', 120, -39), ('Gliese-581g', 180, -12), ('Mercury', 74.8, 167), ('Earth', 510.1, 15), ('Mars', 144.8, -65), ('Jupiter', 61420, -110), ('Saturn', 42700, -140), ('Uranus', 8083, -195), ('Neptune', 7618, -200), ('Kepler-22b', 900, 22), ('LV-426', 110, -45);",
   "CREATE TABLE aliens (alien_id INT PRIMARY KEY AUTO_INCREMENT, species VARCHAR(45) NOT NULL, aggression ENUM('hostile', 'neutral', 'friendly')DEFAULT 'neutral', habitat ENUM('water', 'surface', 'airborne'), planet_id int, CONSTRAINT fk_planet FOREIGN KEY(planet_id) REFERENCES planets(planet_id));",
-  "INSERT INTO aliens (species, aggression, habitat, planet_id) VALUES ('Xenomorph', 'hostile', 'surface', 13),('Yautja', 'hostile', 'surface', 12),('Na vi', 'friendly', 'surface', 12),('Fremen', 'neutral', 'surface', 14),('Wampa', 'hostile', 'surface', 15),('Tauntaun', 'friendly', 'surface', 15),('Cyberman', 'hostile', 'surface', 5),('Keplerian Glider', 'friendly', 'airborne', 9),('Proxima Depth-Dweller', 'neutral', 'water', 10),('Gliese Ice-Crawler', 'neutral', 'surface', 11),('Mercurial Silicon-Mite', 'neutral', 'surface', 1),('Venusian Acid-Skiff', 'neutral', 'airborne', 2), ('Martian Microbe', 'friendly', 'surface', 4),('Europan Aqua-Fiend', 'hostile', 'water', 5),('Titanium Aero-Whale', 'friendly', 'airborne', 6),('Uranian Vapor-Drifter', 'neutral', 'airborne', 7),('Neptunian Abyssal', 'hostile', 'water', 8),('Zoltan Energy-Being', 'friendly', 'airborne', 3), ('Klingon Warrior', 'hostile', 'surface', 14), ('Commander Keen', 'friendly', 'surface', 3)",
+  "INSERT INTO aliens (species, aggression, habitat, planet_id) VALUES ('Zoltan Energy-Being', 'friendly', 'airborne', 8), ('Europan Aqua-Fiend', 'hostile', 'water', 11), ('Martian Microbe', 'friendly', 'surface', 9), ('Gliese Ice-Crawler', 'neutral', 'surface', 6), ('Cyberman', 'hostile', 'surface', 11),('Na vi', 'friendly', 'surface', 4), ('ET', 'friendly', 'surface', 8), ('Xenomorph', 'hostile', 'surface', 15), ('Proxima Depth-Dweller', 'neutral', 'water', 5), ('Grey', 'neutral', 'surface', 8), ('Yautja', 'hostile', 'surface', 3), ('Klingon Warrior', 'hostile', 'surface', 3), ('Wampa', 'hostile', 'surface', 1), ('Tauntaun', 'friendly', 'surface', 1), ('Keplerian Glider', 'friendly', 'airborne', 14), ('Mercurial Silicon-Mite', 'neutral', 'surface', 7), ('Venusian Acid-Skiff', 'neutral', 'airborne', 2), ('Titanium Aero-Whale', 'friendly', 'airborne', 11), ('Uranian Vapor-Drifter', 'neutral', 'airborne', 12), ('Neptunian Abyssal', 'hostile', 'water', 13);",
 ];
 
 console.log("Seed script running");
@@ -25,7 +25,6 @@ const adminDb = mysql.createPool({
 const connection = await adminDb.getConnection();
 
 try {
-
   console.log("Resetting database...");
 
   await connection.query("DROP DATABASE IF EXISTS alienplanets");
@@ -36,7 +35,9 @@ try {
 
   for (const statement of dbInitStatements) {
     await connection.query(statement);
-    console.log(`Executed: ${statement.slice(0, 75)} ${statement.length > 75 && "..."}`);
+    console.log(
+      `Executed: ${statement.slice(0, 75)} ${statement.length > 75 && "..."}`,
+    );
   }
 
   console.log("Database seeded!");
@@ -46,4 +47,3 @@ try {
   if (connection) connection.release();
   process.exit(0);
 }
-
