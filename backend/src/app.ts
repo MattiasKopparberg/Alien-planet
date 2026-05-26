b1b2860661916ec087077753a4701d221524751f
Linnéa Toth <linnea@liroit.com>
2026-05-26 15:10:28 +0200

    rm trailing slash

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173/", //To be replaced with variable
+    origin: "http://localhost:5173", //To be replaced with variable
   }),
 );
 
