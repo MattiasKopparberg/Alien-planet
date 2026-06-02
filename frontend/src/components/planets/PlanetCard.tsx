9a24bbf2f75fa1d2aab2ce8839fde3d212512933
Rebecka Johansson <rebeckaemmajohansson@gmail.com>
2026-06-02 13:29:04 +0200

    updated AlienCard and PlanetCard to fetch images from backend

--- frontend/src/components/planets/PlanetCard.tsx
+++ frontend/src/components/planets/PlanetCard.tsx
@@ -3,8 +3,9 @@ import type { Planet } from "../../types/types";
 export default function PlanetCard({ planet }: { planet: Planet }) {
   return (
     <div className="planet-card">
-      <img className = "h-50 w-50 object-cover"
-        src={`/images/planets/${planet.planet_id}.png`}
+      <img
+        className="h-50 w-50 object-cover"
+        src={`http://localhost:3000/api/planets/${planet.planet_id}/image`} // src fetches image directly from backend URL, no separate fetch needed
         alt={planet.name}
         onError={(e) => {
           e.currentTarget.src = "/Planet-fallback.jpg";
