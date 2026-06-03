616224b050d1a9f4fd03bfcdb5b8d7bd3750a14c
Rebecka Johansson <rebeckaemmajohansson@gmail.com>
2026-06-03 11:37:05 +0200

    added responsive layout for list component

--- frontend/src/components/List.tsx
+++ frontend/src/components/List.tsx
@@ -6,10 +6,7 @@ interface ListProps<T> {
 
 export default function List<T>({ items, renderItem }: ListProps<T>) {
   return (
-    <ul
-      className="grid grid-cols-4 gap-6 px-30
-     py-20 justify-items-center"
-    >
+    <ul className="flex flex-wrap justify-center gap-7 py-20 px-8">
       {/* loopar igenom arrayen, anropar renderItem med ett planet/alien-objekt och renderar ett Card */}
       {items.map((item, index) => (
         <li key={index}>{renderItem(item)}</li>
